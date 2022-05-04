8c0f16d6846da7fdea49fa0b4ef269bfdb5c68e3
simharuk2021 <simharuk@yahoo.co.uk>
2022-05-04 09:13:25 +0000

    code functioning including days

--- front_end_api/application/routes.py
+++ front_end_api/application/routes.py
@@ -11,6 +11,4 @@ def home():
     result = {'numbers_api': numbers_api.text} 
     message = requests.post('http://rollover_api:5000/get_message', json = result)
     
-
-    # return jsonify(f"{numbers_api} {day_api} {message}")
     return jsonify(numbers_api = numbers_api.text, message = message.text, day_api = day_api.text)
