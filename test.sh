ed3734b7d3f2ea7dd1a429b1ecee2a199e59cba4
simharuk2021 <simharuk@yahoo.co.uk>
2022-05-05 15:28:57 +0000

    script change

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ declare -a directories=("front_end_api" "day_api" "numbers_api" "rollover_api")
 for dir in "${directories[@]}"
 do
   cd ${dir}
-  apt-get update
-  apt-get install python3 
-  apt-get install python3-pip 
-  apt-get install python3-venv
+  sudo apt-get update
+  sudo apt-get install python3 
+  sudo apt-get install python3-pip 
+  sudo apt-get install python3-venv
   python3 -m venv venv
   source venv/bin/activate
   pip3 install -r testing.txt
