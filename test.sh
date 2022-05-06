f050126774867dc3393a4a1894ba317bee2fcd4f
simharuk2021 <simharuk@yahoo.co.uk>
2022-05-06 08:55:41 +0000

    pip3 added

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ declare -a directories=("front_end_api" "day_api" "numbers_api" "rollover_api")
 for dir in "${directories[@]}"
 do
   cd ${dir}
-  pip install -r testing.txt
+  pip3 install -r testing.txt
   python3 -m pytest --cov=application --cov-report=xml --junitxml=junit/test-results.xml
   cd ..
 done
